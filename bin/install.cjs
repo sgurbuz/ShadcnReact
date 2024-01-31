ad8b3d0fc7283758888014c17dba86c8096e7c5d
Serhan Gurbuz <Serhan.Gurbuz@logo.com.tr>
2024-01-31 15:59:47 +0300

    update git URL

--- bin/install.cjs
+++ bin/install.cjs
@@ -16,7 +16,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const git_repo = YOUR_GIT_URL;
+const git_repo = 'https://github.com/sgurbuz/ShadcnReact.git';
 
 try {
     fs.mkdirSync(projectPath);
